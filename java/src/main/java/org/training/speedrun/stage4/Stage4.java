abc96afedcf950723654f677218ed4a8ab37d536
Andreas Hütter <andreas.huetter@nagarro.com>
2020-11-21 09:27:23 +0100

    refactor stage 4

--- java/src/main/java/org/training/speedrun/stage4/Stage4.java
+++ java/src/main/java/org/training/speedrun/stage4/Stage4.java
@@ -5,32 +5,42 @@ package org.training.speedrun.stage4;
  */
 public class Stage4 {
 
-    double compute(double[] c, boolean v, boolean b) {
-        double t = 0;
-        for (int i = 0; i < c.length; ++i) {
-            t += c[i];
+    double compute(double[] cart, boolean isVipClient, boolean isBlackFriday) {
+        double overallPrice = 0;
+        int numberOfItems = cart.length;
+        for (int i = 0; i < numberOfItems; ++i) {
+            overallPrice += cart[i];
         }
-        int d = compute(c.length, b, v);
-        double d2 = d * t / 100;
-        return t - d2;
+        int discountPercent = calculateDiscountPercent(numberOfItems, isBlackFriday, isVipClient);
+        double discountMoney = calculateDiscountMoney(overallPrice, discountPercent);
+        return overallPrice - discountMoney;
     }
 
-    int compute(int n, boolean b1, boolean b2) {
-        int p = 0;
-        if (n >= 5 && n < 10) {
-            p += 4;
-        } else if (n >= 10) {
-            p += 10;
-        }
-        if (b1) {
-            // black friday discount
-            p += 20;
+    private double calculateDiscountMoney(double overallPrice, int discountPercent) {
+        double discountMoney = discountPercent * overallPrice / 100;
+        return discountMoney;
+    }
+
+    int calculateDiscountPercent(int numberOfItems, boolean isBlackFriday, boolean isVipClient) {
+        int discountPercent = calculateBaseDiscount(numberOfItems);
+
+        if (isBlackFriday) {
+            discountPercent += 20;
         }
 
-        if (b2) {
-            // vip client
-            p += 5;
+        if (isVipClient) {
+            discountPercent += 5;
         }
-        return p;
+        return discountPercent;
+    }
+
+    private int calculateBaseDiscount(int numberOfItems) {
+        int discountPercent = 0;
+        if (numberOfItems >= 5 && numberOfItems < 10) {
+            discountPercent += 4;
+        } else if (numberOfItems >= 10) {
+            discountPercent += 10;
+        }
+        return discountPercent;
     }
 }
